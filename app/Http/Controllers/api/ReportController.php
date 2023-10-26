6ec6ae76ebeb97d9792b0dda63028f914388ed0e
rwda <rwdaemad27@gmail.com>
2023-10-26 21:52:24 +0300

    update

--- app/Http/Controllers/api/ReportController.php
+++ app/Http/Controllers/api/ReportController.php
@@ -14,6 +14,10 @@ class ReportController extends Controller
     public function index()
     {
         //
+        $report = Report::all();
+
+        return  $report;
+
     }
 
     /**
@@ -22,6 +26,25 @@ class ReportController extends Controller
     public function store(Request $request)
     {
         //
+
+$validator = Validator::make($request->all(), [
+    'subject' => 'required',
+    'problem' => 'required',
+    'image' => 'required',
+
+]);
+
+if ($validator->fails()) {
+
+    return response( $vaidator->errors()->all(), 422);
+}
+
+$report = Report::create($request->all());
+return $report;
+
+
+
+
     }
 
     /**
@@ -30,6 +53,7 @@ class ReportController extends Controller
     public function show(Report $report)
     {
         //
+        return $report;
     }
 
     /**
@@ -37,6 +61,19 @@ class ReportController extends Controller
      */
     public function update(Request $request, Report $report)
     {
+        $validator = Validator::make($request->all(), [
+            'subject' => 'required',
+            'problem' => 'required',
+            'image' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response( $vaidator->errors()->all(), 422);
+        }
+        $report->update($request->all());
+        return $report;
+
+
         //
     }
 
@@ -46,5 +83,7 @@ class ReportController extends Controller
     public function destroy(Report $report)
     {
         //
+        $report->delete();
+        return "deleted successfully";
     }
 }
