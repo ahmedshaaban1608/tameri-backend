b598114c2108b0fa71f3ca5c2fb0b50ba73ae46e
hamza-halawa2020 <h.halawa2020@gmail.com>
2023-11-02 14:34:41 +0200

    add authorization to order controller

--- app/Http/Controllers/api/OrderController.php
+++ app/Http/Controllers/api/OrderController.php
@@ -13,118 +13,138 @@ use Illuminate\Support\Facades\Validator;
 
 class OrderController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+    function __construct()
+    {
+        $this->middleware('auth:sanctum');
+    }
     public function index()
     {
         try {
-            $orders = OrderResource::collection(Order::all());
-            return response()->json(['data'=>$orders], 200);
+            $user = auth()->user();
+            if ($user->type === 'tourist') {
+                // $orders = OrderResource::collection(Order::all());
+                $orders = OrderResource::collection(Order::where('tourist_id', $user->id)->get());
+                return response()->json(['data' => $orders], 200);
+            } elseif ($user->type === 'tourguide') {
+                $orders = OrderResource::collection(Order::where('tourguide_id', $user->id)->get());
+                return response()->json(['data' => $orders], 200);
+            } else {
+                return response()->json(['message' => 'Only tourists or tourguides are allowed to view their orders.'], 403);
+            }
         } catch (\Exception $e) {
             return response()->json(['message' => 'An error occurred while retrieving the data.'], 500);
-                }
-
+        }
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        
-            $validator = Validator::make($request->all(), [
-                'tourist_id' => 'required|numeric',
-                'tourguide_id' => 'required|numeric',
-                'comment' => 'required|string',
-                // 'phone' => 'required',
-                'phone' => 'required|unique:tourists|regex:/^\+?\d{7,14}$/',
-                'from' => 'required|date',
-                'to' => 'required|date',
-                'total' => 'required|numeric',
-                'city' => 'required|string',
-            ]);
-            if ($validator->fails()) {
-                return response()->json(['errors' => $validator->errors()], 422);
-            }
-            try {
-            $tourguide = Tourguide::findOrFail($request->tourguide_id);
-            if (!$tourguide) {
-                return response()->json(['message' => 'Tourguide Id not found'], 404);
-            }
-            $tourist = Tourist::findOrFail($request->tourist_id);
-            if (!$tourist) {
-                return response()->json(['message' => 'Tourist Id not found'], 404);
-            }
+        $validator = Validator::make($request->all(), [
+            // 'tourist_id' => 'required|numeric',
+            'tourguide_id' => 'required|numeric',
+            'comment' => 'required|string',
+            'phone' => 'required|unique:tourists|regex:/^\+?\d{7,14}$/',
+            'from' => 'required|date',
+            'to' => 'required|date',
+            'total' => 'required|numeric',
+            'city' => 'required|string',
+        ]);
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+        try {
+            $user = auth()->user();
+            if ($user->type === 'tourist') {
+                $tourguide = Tourguide::findOrFail($request->tourguide_id);
+                if (!$tourguide) {
+                    return response()->json(['message' => 'Tourguide Id not found'], 404);
+                }
+                // $tourist = Tourist::findOrFail($request->tourist_id);
+                // if (!$tourist) {
+                //     return response()->json(['message' => 'Tourist Id not found'], 404);
+                // }
+                $request->merge(['tourist_id' => $user->id]);
 
-            $order = Order::create($request->all());
-
-            return response()->json(['message'=>'Order created successfully','data' => new OrderResource($order)], 201);
+                $order = Order::create($request->all());
+                return response()->json(['message' => 'Order created successfully', 'data' => new OrderResource($order)], 201);
+            } else {
+                return response()->json(['message' => 'Only tourists are allowed to create orders.'], 403);
+            }
         } catch (\Exception $e) {
             return response()->json(['message' => 'An error occurred while creating the order.'], 500);
         }
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(Order $order)
     {
         try {
-            return response()->json(new OrderResource($order), 200);
+            $user = auth()->user();
+            if ($user->type === 'tourguide' && $order->tourguide_id === $user->id) {
+
+                return response()->json(new OrderResource($order), 200);
+            } elseif ($user->type === 'tourist' && $order->tourist_id === $user->id) {
+
+                return response()->json(new OrderResource($order), 200);
+            } else {
+                return response()->json(['message' => 'You are not allowed to view this order.'], 403);
+            }
         } catch (\Exception $e) {
             return response()->json(['message' => 'An error occurred while retrieving the order.'], 500);
         }
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, Order $order)
     {
-        
-            $validator = Validator::make($request->all(), [
-                'tourist_id' => 'required|numeric',
-                'tourguide_id' => 'required|numeric',
-                'comment' => 'required|string',
-                // 'phone' => 'required',
-                'phone' => 'required|unique:tourists|regex:/^\+?\d{7,14}$/',
-                'from' => 'required|date',
-                'to' => 'required|date',
-                'total' => 'required|numeric',
-                'city' => 'required|string',
-            ]);
+        $validator = Validator::make($request->all(), [
+            // 'tourist_id' => 'required|numeric',
+            // 'tourguide_id' => 'required|numeric',
+            'comment' => 'required|string',
+            // 'phone' => 'required',
+            'phone' => 'required|unique:tourists|regex:/^\+?\d{7,14}$/',
+            'from' => 'required|date',
+            'to' => 'required|date',
+            'total' => 'required|numeric',
+            'city' => 'required|string',
+        ]);
 
-            if ($validator->fails()) {
-                return response()->json(['errors' => $validator->errors()], 422);
-            }
-            try {
-            $tourguide = Tourguide::findOrFail($request->tourguide_id);
-            if (!$tourguide) {
-                return response()->json(['message' => 'Tourguide Id not found'], 404);
-            }
-            $tourist = Tourist::findOrFail($request->tourist_id);
-            if (!$tourist) {
-                return response()->json(['message' => 'Tourist Id not found'], 404);
-            }
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+        try {
+            $user = auth()->user();
+            if ($user->type === 'tourist' && $order->tourist_id === $user->id && $order->status === 'pending') {
 
-            $order->update($request->all());
+                // $tourguide = Tourguide::findOrFail($request->tourguide_id);
+                // if (!$tourguide) {
+                //     return response()->json(['message' => 'Tourguide Id not found'], 404);
+                // }
+                // $tourist = Tourist::findOrFail($request->tourist_id);
+                // if (!$tourist) {
+                //     return response()->json(['message' => 'Tourist Id not found'], 404);
+                // }
 
-            return response()->json(['message' => 'Order updated successfully', 'data' => new OrderResource($order)], 200);
+                $order->update($request->all());
+                return response()->json(['message' => 'Order updated successfully', 'data' => new OrderResource($order)], 200);
+            } else {
+                return response()->json(['message' => 'You are not allowed to update this order.'], 403);
+            }
         } catch (\Exception $e) {
             return response()->json(['message' => 'An error occurred while updating the order.'], 500);
         }
-
     }
-
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(Order $order)
     {
         try {
-            $order->delete();
-            return response()->json(['message' => 'Order deleted successfully'], 200);
+            $user = auth()->user();
+            if ($user->type === 'tourist') {
+                if ($user->id === $order->tourist_id) {
+                    $order->delete();
+                    return response()->json(['message' => 'Order deleted successfully'], 200);
+                } else {
+                    return response()->json(['message' => 'only the Owner Of The order is Allowed to Delete.'], 403);
+                }
+            } else {
+                return response()->json(['message' => 'Only tourists are allowed to delete orders.'], 403);
+            }
         } catch (\Exception $e) {
             return response()->json(['message' => 'An error occurred while deleting the order.'], 500);
         }
--- app/Http/Controllers/api/ReviewController.php
+++ app/Http/Controllers/api/ReviewController.php
@@ -12,56 +12,38 @@ use Illuminate\Support\Facades\Validator;
 
 class ReviewController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     function __construct()
     {
         $this->middleware('auth:sanctum')->except(['index', 'show']);
-        ;
-
     }
     public function index()
     {
-        //
         try {
             $review = ReviewResource::collection(Review::all());
             return response()->json(['data' => $review], 200);
-
         } catch (\Exception $e) {
             return response()->json(['message' => 'An error occurred while retrieving the data.'], 500);
         }
-
-
-
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        //
-
         $validator = Validator::make($request->all(), [
-            'tourist_id' => 'required|numeric',
+            // 'tourist_id' => 'required|numeric',
             'tourguide_id' => 'required|numeric',
             'title' => 'required|string',
             'comment' => 'required|string',
             'stars' => 'required|in:1,2,3,4,5',
-
         ]);
-
         if ($validator->fails()) {
-
             return response($validator->errors()->all(), 422);
         }
-
         try {
             $user = auth()->user();
             if ($user->type === 'tourist') {
                 $tourguide = Tourguide::findOrFail($request->tourguide_id);
-                $tourist = Tourist::findOrFail($request->tourist_id);
+                // $tourist = Tourist::findOrFail($request->tourist_id);
+                $request->merge(['tourist_id' => $user->id]);
                 $review = Review::create($request->all());
                 return response()->json(["message" => "Review created successfully", 'data' => new ReviewResource($review)], 200);
             } else {
@@ -72,66 +54,58 @@ class ReviewController extends Controller
         }
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(Review $review)
     {
-        //
         try {
             return response()->json(new ReviewResource($review), 200);
-
         } catch (\Exception $e) {
             return response()->json(['message' => 'An error occurred while retrieving the data.'], 500);
         }
-
     }
+    //if the review will update by the only tourist who made it
+    // public function update(Request $request, Review $review)
+    // {
+    //     $validator = Validator::make($request->all(), [
+    //         // 'tourist_id' => 'required|numeric',
+    //         'tourguide_id' => 'required|numeric',
+    //         'title' => 'required',
+    //         'comment' => 'required|string',
+    //         'stars' => "required|in:1,2, 3, 4, 5",
+    //         'status' => "required|in:pending,confirmed,declined"
+    //     ]);
+    //     if ($validator->fails()) {
+    //         return response($validator->errors()->all(), 422);
+    //     }
+    //     try {
+    //         $user = auth()->user();
+    //         if ($user->type === 'tourist') {
+    //             if ($user->id === $review->tourist_id) {
+    //                 $tourguide = Tourguide::findOrFail($request->tourguide_id);
+    //                 // $tourist = Tourist::findOrFail($request->tourist_id);
+    //                 $request->merge(['tourist_id' => $user->id]);
 
-    /**
-     * Update the specified resource in storage.
-     */
+    //                 $review->update($request->all());
+    //                 return response()->json(["message" => "Review updated successfully", 'data' => new ReviewResource($review)], 200);
+    //             } else {
+    //                 return response()->json(['message' => 'only the Owner Of The Review is Allowed to make updates.'], 403);
+    //             }
+    //         } else {
+    //             return response()->json(['message' => 'Only tourists are allowed to update reviews.'], 403);
+    //         }
+    //     } catch (\Exception $e) {
+    //         return response()->json(['message' => 'An error occurred while updating the review'], 500);
+    //     }
+    // }
+
+    //no one can update the review
     public function update(Request $request, Review $review)
     {
-        $validator = Validator::make($request->all(), [
-            'tourist_id' => 'required|numeric',
-            'tourguide_id' => 'required|numeric',
-            'title' => 'required',
-            'comment' => 'required|string',
-            'stars' => "required|in:1,2, 3, 4, 5",
-            'status' => "required|in:pending,confirmed,declined"
-
-        ]);
-
-        if ($validator->fails()) {
-
-            return response($validator->errors()->all(), 422);
-        }
-        try {
-            $user = auth()->user();
-            if ($user->type === 'tourist') {
-                if ($user->id === $review->tourist_id) {
-                    $tourguide = Tourguide::findOrFail($request->tourguide_id);
-                    $tourist = Tourist::findOrFail($request->tourist_id);
-                    $review->update($request->all());
-                    return response()->json(["message" => "Review updated successfully", 'data' => new ReviewResource($review)], 200);
-                } else {
-                    return response()->json(['message' => 'You are not allowed to update this review.'], 403);
-                }
-            } else {
-                return response()->json(['message' => 'Only tourists are allowed to update reviews.'], 403);
-            }
-        } catch (\Exception $e) {
-            return response()->json(['message' => 'An error occurred while updating the review'], 500);
-        }
+        return response()->json(['message' => 'Review updates are not allowed.'], 403);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
+
     public function destroy(Review $review)
     {
-        //
-
         try {
             $user = auth()->user();
             if ($user->type === 'tourist') {
@@ -139,7 +113,7 @@ class ReviewController extends Controller
                     $review->delete();
                     return response()->json("deleted successfully", 200);
                 } else {
-                    return response()->json(['message' => 'You are not allowed to delete this review.'], 403);
+                    return response()->json(['message' => 'only the Owner Of The Review is Allowed to Delete.'], 403);
                 }
             } else {
                 return response()->json(['message' => 'Only tourists are allowed to delete reviews.'], 403);
@@ -147,6 +121,5 @@ class ReviewController extends Controller
         } catch (\Exception $e) {
             return response()->json(['message' => 'An error occurred while deleting the review'], 500);
         }
-
     }
 }
